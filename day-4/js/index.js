39811389019fc0e6b705340b3161d0df4409161f
foramtrada232 <foramtrada232@gmail.com>
2019-02-21 19:31:56 +0530

    css  task

---
+++ day-4/js/index.js
@@ -0,0 +1,11 @@
+
+
+mdc.textField.MDCTextField.attachTo(document.querySelector('.name'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.address'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.country'));
+mdc.switchControl.MDCSwitch.attachTo(document.querySelector('.switch1'));
+mdc.switchControl.MDCSwitch.attachTo(document.querySelector('.switch2'));
+// mdc.buttonRipple.MDCRipple.attachTo(document.querySelector('.button1'));
+// mdc.buttonRipple.MDCRipple.attachTo(document.querySelector('.button2'));
+// mdc.buttonRipple.MDCRipple.attachTo(document.querySelector('.button3'));
+mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'));
